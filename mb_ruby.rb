0cec0475f604dfc5ac5bf46eaf8895d007ac80c5
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-01-11 17:51:23 -0600

    refactoring of the following classes: Bot / BotCommands / HelloText

--- mb_ruby.rb
+++ mb_ruby.rb
@@ -1,6 +1,2 @@
 require 'slack-ruby-bot'
-require 'mb_ruby/commands/insult_member_team'
-require 'mb_ruby/commands/screenshot_page'
-require 'mb_ruby/commands/wolfram_research'
-require 'mb_ruby/commands/say_hello'
 require 'mb_ruby/bot'
--- mb_ruby/bot.rb
+++ mb_ruby/bot.rb
@@ -1,18 +1,65 @@
-
 require 'slack-ruby-bot'
 require 'httparty'
+require 'open-uri'
+require 'wolfram'
+Wolfram.appid= ENV["WOLFRAM_APPID"]
+require 'json'
+require 'screencap'
+require 'imgur2'
 
 module MbRuby
   class Bot < SlackRubyBot::Bot    
     help do
       title 'My Ruby Bot'
-      desc 'This office bot, which will heplp you as an office assistant'
+      desc 'This office bot, which will hepl you as an office assistant'
       
       command :say_hello do
         title 'say_hello'
-        desc "Welcome command, to recieve you properly"
-        long_desc "Welcome command, to recieve you properly"
+        desc "Welcome command, to recieve you properly."
+        long_desc "Welcome command, to recieve you properly."
+      end    
+    end
+  end
+
+  class BotCommands < SlackRubyBot::Commands::Base
+    command 'say_hello' do |client, data, _match|
+      client.say(channel: data.channel,  text: HelloText.say_hello)
+    end
+
+    command 'insult' do |client, data, _match|
+      result = JSON.parse Http.get('http://quandyfactory.com/insult/json').body
+      insult = result['insult']
+      client.message text: "Hey #{_match[:expression]}, #{insult}", channel: data.channel
+    end
+
+    command 'wolf' do |client, data, _match|
+
+      q = _match[:expression]
+      result = Wolfram::Query.new(q).fetch
+      hash = Wolfram::HashPresenter.new(result).to_hash
+
+      result = ""
+
+      hash.fetch(:pods, {}).each do |key, values|
+        next if values.join("") == ""
+        result << "\n" + key + "\n"
+        result << values.join("\n")
+      end
+
+      if result != ""
+        client.message text: result, channel: data.channel
+      else
+        client.message text: "
+          No results found for #{q}. \n
+          Try https://duckduckgo.com/?q=#{URI.escape(q)}+!google",
+          channel: data.channel
       end
     end
+  end
+
+  class HelloText
+    def self.say_hello
+      'Hello! This is a Bot test!'
+    end  
   end
 end

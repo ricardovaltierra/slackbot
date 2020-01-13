586d5e203b52eb4e4c41a544d490a9face4c3ef5
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-01-13 11:54:59 -0600

    methods unused for the bot removed / refactoring bot-rb and config.ru

--- config.ru
+++ config.ru
@@ -1,10 +1,4 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'wolfram'
-Wolfram.appid= ENV["WOLFRAM_APPID"]
-
-require 'dotenv'
-Dotenv.load
-
 require 'mb_ruby'
-MbRuby::Bot.run
+MbRuby::App.instance.run
--- mb_ruby/bot.rb
+++ mb_ruby/bot.rb
@@ -10,76 +10,13 @@ require 'mechanize'
 require 'screencap'
 require 'imgur2'
 
-module RubyBot
+module MbRuby
   class App < SlackRubyBot::App
   end
 
-  class Hiiii < SlackRubyBot::Commands::Base
-    command 'hi' do |client, data, _match|
-      client.message text: 'hiiiiiiiiiiii', channel: data.channel
-    end
-  end
+  class WFSearch < SlackRubyBot::Commands::Base
 
-  class Image < SlackRubyBot::Commands::Base
-
-    command 'imageme' do |client, data, _match|
-
-
-      Mechanize.new { |agent|
-        agent.user_agent_alias = 'Mac Safari'
-      }
-      .get("http://theironyard.com/about/team/")
-      .search(".bio h2").select{|tag| tag.text =~ /#{_match[:expression]}/i}.map do |tag|
-        {
-          name: tag.text,
-          src: "http://theironyard.com/" + tag.parent.parent.search("img").attr("src").value
-        }
-      end
-      .each do |args|
-        client.message text: args[:name], channel: data.channel
-        client.message text: args[:src], channel: data.channel
-      end
-
-
-    end
-  end
-
-  class Screenshot < SlackRubyBot::Commands::Base
-
-    command 'screenshot' do |client, data, _match|
-
-      url = URI(_match[:expression].match(/\<(.*)\>/)[1]) rescue nil
-
-      if url
-        fetcher_object = Screencap::Fetcher.new(url.to_s)
-        screenshot = fetcher_object.fetch width: 1700, height: 850
-        imgur_client = Imgur2.new ENV['IMGUR_API']
-        url = imgur_client.upload screenshot
-        image_url = url["upload"]["links"]["original"]
-        client.message text: image_url, channel: data.channel
-      else
-        client.message text: _match[:expression] + "isn't a URL, yo", channel: data.channel
-
-      end
-    end
-  end
-
-  class Insults < SlackRubyBot::Commands::Base
-
-    command 'insult' do |client, data, _match|
-
-      result = JSON.parse Http.get('http://quandyfactory.com/insult/json').body
-      insult = result['insult']
-      client.message text: "Hey #{_match[:expression]}, #{insult}", channel: data.channel
-
-    end
-
-  end
-
-  class WolframSearch < SlackRubyBot::Commands::Base
-
-
-    command 'wolf' do |client, data, _match|
+    command 'Could' do |client, data, _match|
 
       q = _match[:expression]
       result = Wolfram::Query.new(q).fetch
@@ -101,9 +38,6 @@ module RubyBot
           Try https://duckduckgo.com/?q=#{URI.escape(q)}+!google",
           channel: data.channel
       end
-
     end
   end
 end
-
-RubyBot::App.instance.run
